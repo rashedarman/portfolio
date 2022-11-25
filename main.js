4d776ba39ceb829a90a1ff14bdccf553f0bd69bb
Rashed Arman <rashedaraman21@gmail.com>
2022-11-25 13:42:20 +0600

    Add mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const hamburger = document.getElementById('hamburger');
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
 const navMenu = document.getElementById('menu');
+const navLinks = document.querySelectorAll('#menu > a');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
+openBtn.addEventListener('click', () => {
+  navMenu.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
+navLinks.forEach((el) => {
+  el.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('show');
 });
